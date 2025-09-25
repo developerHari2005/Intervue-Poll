b0737d38a93cd8c69ea9bae2bfc3caa3cd168455
developerHari2005 <hackinghackerhari@gmail.com>
2025-09-25 08:25:19 +0530

    Updated API URL

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8001/api';
+const API_BASE_URL = 'https://intervue-poll-1-laf7.onrender.com/api';
 
 class ApiService {
   async request(endpoint, options = {}) {
